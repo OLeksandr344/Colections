75dbbd3c045bc69a0a92255d9db0fd19a5e4e84e
Olexandr <124339852+OLeksandr344@users.noreply.github.com>
2023-11-17 20:36:38 +0200

    1

---
+++ ColectionsTasks/ColectionsTasks/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using ColectionsTasks;
+using Task = ColectionsTasks.Task;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        TaskManager taskManager = new TaskManager();
+        Task t1 = new Task(1,"jumping");
+        Task t2 = new Task(1, "skiing");
+        taskManager.Add(t1);
+        taskManager.Add(t2);
+        Console.WriteLine(taskManager.Count()); 
+        taskManager.DisplayAllTasks();
+        taskManager.RemoveAt(0);
+        Console.WriteLine(taskManager.Count()); 
+        taskManager.DisplayAllTasks();
+        taskManager.Add(t1);
+        Console.WriteLine(taskManager.Count());
+        taskManager.DisplayAllTasks();
+        Console.WriteLine("------------------------------");
+
+        UserManager userManager = new UserManager();    
+        User user = new User(1,"Person1");
+        userManager.Add(user);
+        userManager.SearchUser(1);
+        userManager.DisplayAllUsers();
+    }
+}
---
+++ ColectionsTasks/ColectionsTasks/Task.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColectionsTasks
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string? Description { get; set; }
+        public Task(int id, string desc)
+        {
+            TaskId = id;
+            Description = desc;
+        }
+
+    }
+    public class TaskManager : List<Task>
+    {
+        public TaskManager()
+        {
+
+        }
+        public void DisplayAllTasks()
+        {
+            foreach (var task in this)
+            {
+                Console.WriteLine($"Task ID: {task.TaskId}, Description: {task.Description}");
+            }
+
+        }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User(int id, string name) 
+        { 
+            Id = id;
+            Name = name;
+        }
+
+    }
+    public class UserManager : List<User>
+    {
+        public UserManager() { }
+        public void DisplayAllUsers()
+        {
+            foreach (var user in this)
+            {
+                Console.WriteLine($"User ID: {user.Id}, Name: {user.Name}");
+            }
+        }
+        public void SearchUser(int id)
+        {
+            
+            foreach (var user in this)
+            {
+                if (id == user.Id)
+                {
+                    Console.WriteLine($"User ID: {user.Id}, Name: {user.Name}");
+                }
+                    
+            }
+        }
+    }
+}
